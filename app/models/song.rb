a858715a897361385be4ac72cd3e46c0b38bd0af
HaydenGriffin <haydengriffin@hotmail.co.uk>
2016-11-26 18:08:11 +0000

    Added regex validation to song length field

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :album
   validates :name, presence: true
+  validates_format_of :length, with: /\A([0-5]?[0-9]):([0-5]?[0-9])\z/, on: :create, :message =>"invalid. Length must be given in the form MM:SS"
 end
